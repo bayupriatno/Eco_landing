f2511eda01cfab8f0b8296c74f20777a31f9e8e2
bayupriatno <bayupriatno007@gmail.com>
2025-07-31 03:36:18 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: true, // Jika butuh Server Actions
+  },
+  images: {
+    domains: ["cdn.jsdelivr.net", "images.unsplash.com"], // sesuaikan jika pakai gambar eksternal
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
