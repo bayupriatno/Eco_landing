fc64c35ebec8019a5e7d213c27726d8c6f622ef4
bayupriatno <bayupriatno007@gmail.com>
2025-07-31 03:28:28 +0700

    update

---
+++ i18n.ts
@@ -0,0 +1,11 @@
+"use client";
+import { getRequestConfig } from "next-intl/server";
+
+export default getRequestConfig(() => ({
+  locales: ["en", "id"],
+  defaultLocale: "en",
+  messages: {
+    en: require("./locales/en.json"),
+    id: require("./locales/id.json"),
+  },
+}));"
